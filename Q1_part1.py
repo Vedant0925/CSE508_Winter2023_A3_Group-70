ce7ecf28afa79b406b2a8514cb7515e833b27fe7
Vedant Patil <74593220+Vedant0925@users.noreply.github.com>
2023-04-25 22:41:13 +0530

    Update Q1_part1.py

--- Q1_part1.py
+++ Q1_part1.py
@@ -1,1 +1,94 @@
+def read_edges(file_path):
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        lines = [line.strip().split() for line in lines if not line.startswith("#")]
 
+    edges = [(int(line[0]), int(line[1])) for line in lines]
+    return edges
+
+def create_adjacency_list(edges):
+    adjacency_list = {}
+    for u, v in edges:
+        if u not in adjacency_list:
+            adjacency_list[u] = []
+        adjacency_list[u].append(v)
+    return adjacency_list
+
+file_path = "/Users/vedant/Downloads/wiki-Vote.txt"
+edges = read_edges(file_path)
+
+
+adj_matrix = create_adjacency_list(edges)
+
+
+# print("Adjacency Matrix:")
+# for row in adj_matrix:
+#     print(row)
+#
+#
+# print("\nEdge List:")
+# for edge in edges:
+#     print(edge)
+
+# Function to calculate the number of nodes
+def number_of_nodes(adjacency_list):
+    nodes = set(adjacency_list.keys())
+    for neighbors in adjacency_list.values():
+        nodes.update(neighbors)
+    return len(nodes)
+
+# Function to calculate the number of edges
+def number_of_edges(edges):
+    return len(edges)
+
+# Functions to calculate in-degree and out-degree
+def in_out_degrees(edges):
+    in_degrees = {}
+    out_degrees = {}
+    for u, v in edges:
+        out_degrees[u] = out_degrees.get(u, 0) + 1
+        in_degrees[v] = in_degrees.get(v, 0) + 1
+    return in_degrees, out_degrees
+
+# Function to calculate the average in-degree and out-degree
+def avg_in_out_degrees(in_degrees, out_degrees, num_nodes):
+    total_in_degree = sum(in_degrees.values())
+    total_out_degree = sum(out_degrees.values())
+    avg_in_degree = total_in_degree / num_nodes
+    avg_out_degree = total_out_degree / num_nodes
+    return avg_in_degree, avg_out_degree
+
+# Functions to find the nodes with maximum in-degree and out-degree
+def max_in_out_degree(in_degrees, out_degrees):
+    max_in_degree_node = max(in_degrees, key=in_degrees.get)
+    max_out_degree_node = max(out_degrees, key=out_degrees.get)
+    return max_in_degree_node, max_out_degree_node
+
+# Function to calculate the network density
+def network_density(num_nodes, num_edges):
+    density = num_edges / (num_nodes * (num_nodes - 1))
+    return density
+
+# Load the dataset
+file_path = "/Users/vedant/Downloads/wiki-Vote.txt"
+edges = read_edges(file_path)
+
+# Create adjacency list representation
+adjacency_list = create_adjacency_list(edges)
+
+# Calculate statistics
+num_nodes = number_of_nodes(adjacency_list)
+num_edges = number_of_edges(edges)
+in_degrees, out_degrees = in_out_degrees(edges)
+avg_in_degree, avg_out_degree = avg_in_out_degrees(in_degrees, out_degrees, num_nodes)
+max_in_degree_node, max_out_degree_node = max_in_out_degree(in_degrees, out_degrees)
+density = network_density(num_nodes, num_edges)
+
+# Print statistics
+print(f"1. Number of Nodes: {num_nodes}")
+print(f"2. Number of Edges: {num_edges}")
+print(f"3. Avg In-degree: {avg_in_degree}")
+print(f"4. Avg. Out-Degree: {avg_out_degree}")
+print(f"5. Node with Max In-degree: {max_in_degree_node}")
+print(f"6. Node with Max out-degree: {max_out_degree_node}")
+print(f"7. The density of the network: {density}")
